219327b6870fd976bdf5ae93c8171e64ca4314ae
Lakshmi Sureshan <lakshmisureshan@gmail.com>
2023-01-10 16:09:56 +0400

    Commit Movie Model and Enum

---
+++ CinemaApp/CinemaApp/Data/MovieCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaApp.Data
+{
+    public enum  MovieCategory
+    {
+        //'Action start index 
+        Action =1 ,
+        Comedy ,
+        Drama,
+        Documentary
+    }
+}
